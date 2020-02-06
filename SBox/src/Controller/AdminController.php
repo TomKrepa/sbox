9cf8dfe9e752428551821e3364988775439c5e01
Tom Krepa <tom.krepa@ynov.com>
2020-02-06 14:40:40 +0100

    add Connexion/Inscription

--- SBox/src/Controller/AdminController.php
+++ SBox/src/Controller/AdminController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/home", name="sign_in")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/register", name="sign_up")
+     */
+    public function register()
+    {
+        return $this->render('admin/register.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
